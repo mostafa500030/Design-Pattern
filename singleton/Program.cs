dd8d57f64a05460e83d249ef55881539fbe4eb9f
mostafa500030 <71846308+mostafa500030@users.noreply.github.com>
2024-01-30 23:35:50 +0200

    Singleton Design Pattern

---
+++ singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Access the singleton instance
+            Singleton singletonInstance1 = Singleton.Instance;
+            Singleton singletonInstance2 = Singleton.Instance;
+
+            // Both instances refer to the same object
+            Console.WriteLine($"Are the instances the same? {singletonInstance1 == singletonInstance2}");
+
+            // Use the singleton instance
+            singletonInstance1.DisplayMessage();
+            singletonInstance2.DisplayMessage();
+
+            // Note: You can't create new instances using the private constructor
+            // Singleton invalidInstance = new Singleton(); // This will result in a compilation error
+        }
+    }
+}
---
+++ singleton/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    public class Singleton
+    {
+        // Private static instance
+        private static Singleton instance;
+
+        // Private constructor to prevent instantiation
+        private Singleton()
+        {
+            Console.WriteLine("Singleton instance created");
+        }
+
+        // Public method to get the singleton instance
+        public static Singleton Instance
+        {
+            get
+            {
+                // Create the instance if it doesn't exist
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+
+                return instance;
+            }
+        }
+
+        // Other methods and properties of the singleton class
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Singleton instance is displaying a message");
+        }
+    }
+}
