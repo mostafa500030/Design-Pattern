2e5c683c46cbfdfff706e22bdbbc80a74cb204c3
mostafa500030 <71846308+mostafa500030@users.noreply.github.com>
2024-01-30 23:36:10 +0200

    Factory Design Pattern

---
+++ Factory/CarClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class CarClient
+    {
+        private readonly ICarFactory carFactory;
+
+        public CarClient(ICarFactory carFactory)
+        {
+            this.carFactory = carFactory ?? throw new ArgumentNullException(nameof(carFactory));
+        }
+
+        public void Run()
+        {
+            ICar car = carFactory.CreateCar();
+            Console.WriteLine("Car created by the factory:");
+            car.DisplayInfo();
+        }
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using the Sedan Car Factory to create a sedan
+            ICarFactory sedanFactory = new SedanCarFactory();
+            CarClient sedanClient = new CarClient(sedanFactory);
+            sedanClient.Run();
+
+            // Using the SUV Car Factory to create an SUV
+            ICarFactory suvFactory = new SUVCarFactory();
+            CarClient suvClient = new CarClient(suvFactory);
+            suvClient.Run();
+        }
+    }
+}
